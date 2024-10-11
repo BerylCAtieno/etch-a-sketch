32d24a71a973ba68cb4bf20eedfc424c510ff9cb
Beryl Atieno <berylatieno30@gmail.com>
2024-10-11 12:08:02 +0300

    add grid items logic

--- script.js
+++ script.js
@@ -35,12 +35,27 @@ gridContainerDiv.style.width = "500px"
 gridContainerDiv.style.height = "500px"
 gridContainerDiv.style.background = "white"
 gridContainerDiv.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.2)"
+gridContainerDiv.style.display = "flex"
+gridContainerDiv.style.flexWrap = "wrap"
 
 navGridContainer.appendChild(gridContainerDiv)
 
 
 function createGrid(size) {
-    // takes size of grid
-    // creates a square grid of divs of size by size
+    const gridItemSize = 500 / size;
 
+    // Create the grid divs
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+                
+        // Apply inline styles to make it fit in the grid
+        gridItem.style.width = `${gridItemSize}px`;
+        gridItem.style.height = `${gridItemSize}px`;
+        gridItem.style.background = "white"
+
+        // Append the grid item to the gridContainer
+        gridContainerDiv.appendChild(gridItem);
+
+    }
 }
